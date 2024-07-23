5214b3944650867f90078f1a931db270ee7eaab1
Anna Miolato <miolatoanna1@gmail.com>
2024-07-23 11:50:12 +0200

    add ng select

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,31 +3,33 @@ import { BrowserModule } from '@angular/platform-browser';
 import {BrowserAnimationsModule, NoopAnimationsModule} from "@angular/platform-browser/animations";
 import {ButtonModule} from "primeng/button";
 import {InputTextModule} from "primeng/inputtext";
+import {CommonModule} from "@angular/common";
+import {HttpClientModule} from "@angular/common/http";
 
 import { AppComponent } from './app.component';
 import { RequestComponent } from './components/request/request.component';
-import { ResultComponent } from './components/result/result.component';
 import { HeaderComponent } from './components/header/header.component';
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 import { FooterComponent } from './components/footer/footer.component';
-import {HttpClientModule} from "@angular/common/http";
+import {NgSelectModule} from "@ng-select/ng-select";
 
 
 @NgModule({
   declarations: [
     AppComponent,
     RequestComponent,
-    ResultComponent,
     HeaderComponent,
     FooterComponent
   ],
   imports: [
     BrowserModule,
+    CommonModule,
     HttpClientModule,
     ButtonModule,
     InputTextModule,
     NoopAnimationsModule,
     BrowserAnimationsModule,
+    NgSelectModule,
     FormsModule,
     ReactiveFormsModule,
   ],
